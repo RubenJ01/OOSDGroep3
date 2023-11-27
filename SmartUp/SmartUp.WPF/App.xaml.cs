e9d0faceeccb5795ab96482173f4204f99718d1a
@s1186359 <s1186359@student.windesheim.nl>
2023-11-27 09:50:37 +0100

    Tussen branch omdat ik fuckte met -f push

--- SmartUp/SmartUp.WPF/App.xaml.cs
+++ SmartUp/SmartUp.WPF/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SmartUp.WPF
+namespace SmartUp.UI
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ SmartUp/SmartUp.WPF/Controller/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace SmartUp.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContentArea.Navigate(new Uri("./View/GradeUpdateScreen.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ContentArea.Navigate(new Uri("./View/GradeStudent.xaml", UriKind.Relative));
+        }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            ContentArea.Navigate(new Uri("./View/SemesterStudent.xaml", UriKind.Relative));
+        }
+    }
+}
