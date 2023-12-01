da8f7d60352c844d57aa1b5617564a99752c44ed
JoelleDoorn <s1183785@student.windesheim.nl>
2023-12-01 10:21:15 +0100

    branch aangemaakt

--- SmartUp/SmartUp.Core/Constants/Constants.cs
+++ SmartUp/SmartUp.Core/Constants/Constants.cs
@@ -8,6 +8,6 @@ namespace SmartUp.Core.Constants
 {
     public class Constants
     {
-        public static string STUDENT_ID = "S000189";
+        public static string STUDENT_ID = "S000002";
     }
 }
