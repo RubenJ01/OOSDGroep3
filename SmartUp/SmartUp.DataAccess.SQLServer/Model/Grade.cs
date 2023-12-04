19d12013991cd8d0095daf7acf182670ae4a97c0
@s1186359 <s1186359@student.windesheim.nl>
2023-12-04 15:54:25 +0100

    Usecase inzien herkansingen

--- SmartUp/SmartUp.DataAccess.SQLServer/Model/Grade.cs
+++ SmartUp/SmartUp.DataAccess.SQLServer/Model/Grade.cs
@@ -13,14 +13,18 @@ namespace SmartUp.DataAccess.SQLServer.Model
         public DateTime PublishedOn { get; set; }
         public string CourseName { get; set; }
         public int Credits { get; set; }
+        public int Attempt { get; set; }
+        public string? DisplayGrade {  get; set; }
 
-        public Grade(decimal grade, bool isDefinitive, DateTime publishedOn, string courseName, int credits)
+        public Grade(decimal grade, bool isDefinitive, DateTime publishedOn, string courseName, int credits, int attempt)
         {
             GradeNumber = grade;
             IsDefinitive = isDefinitive;
             PublishedOn = publishedOn;
             CourseName = courseName;
             Credits = credits;
+            Attempt = attempt;
+            DisplayGrade = $"Kans {attempt}: {grade}";
         }
     }
 }
